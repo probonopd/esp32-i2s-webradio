fd198364b08521ed25d4ec8e9e928901d98c581e
probonopd <probonopd@users.noreply.github.com>
2023-02-06 22:48:45 +0100

    Remove TFT code

--- src/esp32-i2s-webradio.ino
+++ src/esp32-i2s-webradio.ino
@@ -4,15 +4,11 @@
 #include <Preferences.h>
 #include <SPI.h>
 #include <WiFi.h>
-#include "tft.h"   //see my repository at github "https://github.com/schreibfaul1/ESP32-TFT-Library-ILI9486"
-#include "Audio.h" //see my repository at github "https://github.com/schreibfaul1/ESP32-audioI2S"
-#include "IR.h"    //see my repository at github "ESP32-IR-Remote-Control"
+
+#include "Audio.h" // https://github.com/schreibfaul1/ESP32-audioI2S
+#include "IR.h"    // https://github.com/schreibfaul1/ESP32-IR-Remote-Control"
 
 
-#define TFT_CS        22
-#define TFT_DC        21
-#define TP_CS         16
-#define TP_IRQ        39
 #define SPI_MOSI      23
 #define SPI_MISO      19
 #define SPI_SCK       18
@@ -22,8 +18,6 @@
 #define IR_PIN        34
 
 Preferences pref;
-TFT tft;        // @suppress("Abstract class cannot be instantiated")
-TP tp(TP_CS, TP_IRQ);
 Audio audio;
 IR ir(IR_PIN);  // do not change the objectname, it must be "ir"
 
@@ -53,69 +47,19 @@ int8_t  cur_btn      =-1;   //current button (, -1 means idle)
 enum action{VOLUME_UP=0, VOLUME_DOWN=1, STATION_UP=2, STATION_DOWN=3};
 enum staus {RELEASED=0, PRESSED=1};
 
-struct _btns{
-    uint16_t x; //PosX
-    uint16_t y; //PosY
-    uint16_t w; //Width
-    uint16_t h; //Hight
-    uint8_t  a; //Action
-    uint8_t  s; //Status
-};
-typedef _btns btns;
 
-btns btn[4];
 
-//**************************************************************************************************
-//                                              G U I                                              *
-//**************************************************************************************************
-void draw_button(btns b){
-    uint16_t color=TFT_BLACK;
-    uint8_t r=4, o=r*3;
-    if(b.s==RELEASED) color=TFT_GOLD;
-    if(b.s==PRESSED)  color=TFT_DEEPSKYBLUE;
-    tft.drawRoundRect(b.x, b.y, b.w, b.h, r, color);
-    switch(b.a){
-        case VOLUME_UP:
-            tft.fillTriangle(b.x+b.w/2, b.y+o, b.x+o, b.y+b.h-o, b.x+b.w-o, b.y+b.h-o, color); break;
-        case VOLUME_DOWN:
-            tft.fillTriangle(b.x+o, b.y+o, b.x+b.w/2, b.y+b.h-o, b.x+b.w-o, b.y+o, color); break;
-        case STATION_UP:
-            tft.fillTriangle(b.x+o, b.y+o, b.x+o, b.y+b.h-o, b.x+b.w-o, b.y+b.h/2, color); break;
-        case STATION_DOWN:
-            tft.fillTriangle(b.x+b.w-o, b.y+o, b.x+o, b.y+b.h/2, b.x+b.w-o, b.y+b.h-o, color); break;
-    }
-}
 void write_stationNr(uint8_t nr){
-    tft.fillRect(80, 250, 80, 60, TFT_BLACK);
     String snr = String(nr);
     if(snr.length()<2) snr = "0"+snr;
-    tft.setCursor(98, 255);
-    tft.setFont(Times_New_Roman66x53);
-    tft.setTextColor(TFT_YELLOW);
-    tft.print(snr);
 }
 void write_volume(uint8_t vol){
-    tft.fillRect(320, 250, 80, 60, TFT_BLACK);
     String svol = String(vol);
     if(svol.length()<2) svol = "0"+svol;
-    tft.setCursor(338, 255);
-    tft.setFont(Times_New_Roman66x53);
-    tft.setTextColor(TFT_YELLOW);
-    tft.print(svol);
 }
 void write_stationName(String sName){
-    tft.fillRect(0, 0, 480, 100, TFT_BLACK);
-    tft.setFont(Times_New_Roman43x35);
-    tft.setTextColor(TFT_CORNSILK);
-    tft.setCursor(20, 20);
-    tft.print(sName);
 }
 void write_streamTitle(String sTitle){
-    tft.fillRect(0, 100, 480, 150, TFT_BLACK);
-    tft.setFont(Times_New_Roman43x35);
-    tft.setTextColor(TFT_LIGHTBLUE);
-    tft.setCursor(20, 100);
-    tft.print(sTitle);
 }
 void volume_up(){
     if(cur_volume < max_volume){
@@ -135,7 +79,6 @@ void station_up(){
     if(cur_station < max_stations-1){
         cur_station++;
         write_stationNr(cur_station);
-        tft.fillRect(0, 0, 480, 250, TFT_BLACK);
         audio.connecttohost(stations[cur_station].c_str());
         pref.putShort("station", cur_station);} // store the current station in nvs
 }
@@ -143,7 +86,6 @@ void station_down(){
     if(cur_station > 0){
         cur_station--;
         write_stationNr(cur_station);
-        tft.fillRect(0, 0, 480, 250, TFT_BLACK);
         audio.connecttohost(stations[cur_station].c_str());
         pref.putShort("station", cur_station);} // store the current station in nvs
 }
@@ -153,10 +95,6 @@ void station_down(){
 //                                           S E T U P                                             *
 //**************************************************************************************************
 void setup() {
-    btn[0].x= 20; btn[0].y=250; btn[0].w=60; btn[0].h=60; btn[0].a=STATION_DOWN; btn[0].s=RELEASED;
-    btn[1].x=160; btn[1].y=250; btn[1].w=60; btn[1].h=60; btn[1].a=STATION_UP;   btn[1].s=RELEASED;
-    btn[2].x=260; btn[2].y=250; btn[2].w=60; btn[2].h=60; btn[2].a=VOLUME_UP;    btn[2].s=RELEASED;
-    btn[3].x=400; btn[3].y=250; btn[3].w=60; btn[3].h=60; btn[3].a=VOLUME_DOWN;  btn[3].s=RELEASED;
     max_stations= sizeof(stations)/sizeof(stations[0]); log_i("max stations %i", max_stations);
     Serial.begin(115200);
     SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
@@ -173,12 +111,7 @@ void setup() {
     WiFi.begin(ssid.c_str(), password.c_str());
     while (WiFi.status() != WL_CONNECTED) {delay(1500); Serial.print(".");}
     log_i("Connected to %s", WiFi.SSID().c_str());
-    tft.begin(TFT_CS, TFT_DC, SPI_MOSI, SPI_MISO, SPI_SCK);
-    tft.setFrequency(20000000);
-    tft.setRotation(3);
-    tp.setRotation(3);
-    tft.setFont(Times_New_Roman43x35);
-    tft.fillScreen(TFT_BLACK);
+
     ir.begin();  // Init InfraredDecoder
     audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
     audio.setVolume(cur_volume); // 0...21
@@ -193,7 +126,6 @@ void setup() {
 void loop()
 {
     audio.loop();
-    tp.loop();
     ir.loop();
 }
 //**************************************************************************************************
@@ -240,20 +172,14 @@ void ir_res(uint32_t res){
     if(res < max_stations){
         cur_station = res;
         write_stationNr(cur_station);
-        tft.fillRect(0, 0, 480, 250, TFT_BLACK);
         audio.connecttohost(stations[cur_station].c_str());
         pref.putShort("station", cur_station);} // store the current station in nvs
     else{
-        tft.fillRect(0, 0, 480, 250, TFT_BLACK);
         audio.connecttohost(stations[cur_station].c_str());
     }
 }
 void ir_number(const char* num){
-    tft.fillRect(0, 0, 480, 250, TFT_BLACK);
-    tft.setTextSize(7);
-    tft.setTextColor(TFT_CORNSILK);
-    tft.setCursor(50, 70);
-    tft.print(num);
+
 }
 void ir_key(const char* key){
     switch(key[0]){
