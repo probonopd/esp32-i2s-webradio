62a799413b90d4c2928fb80247b24a2bd766639e
probonopd <probonopd@users.noreply.github.com>
2023-02-06 23:17:59 +0100

    https://github.com/lbussy/AsyncWiFiManager.git#development

--- src/esp32-i2s-webradio.ino
+++ src/esp32-i2s-webradio.ino
@@ -8,9 +8,9 @@
 #include "Audio.h" // https://github.com/schreibfaul1/ESP32-audioI2S
 #include "IR.h"    // https://github.com/schreibfaul1/ESP32-IR-Remote-Control"
 
-#include <WiFiManager.h>
+#include <AsyncWiFiManager.h>          //https://github.com/lbussy/AsyncWiFiManager WiFi Configuration Magic
 
-WiFiManager wifiManager;
+AsyncWiFiManager AsyncWiFiManager;
 
 #define SPI_MOSI      23
 #define SPI_MISO      19
@@ -108,7 +108,7 @@ void setup() {
         cur_volume = pref.getShort("volume");
     }
 
-    wifiManager.autoConnect("AutoConnectAP");
+    AsyncWiFiManager.autoConnect();
         
     while (WiFi.status() != WL_CONNECTED) {delay(1500); Serial.print(".");}
     log_i("Connected to %s", WiFi.SSID().c_str());
